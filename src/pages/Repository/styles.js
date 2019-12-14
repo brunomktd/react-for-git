30cf76e0f8f0aa3780fec4ac7a7a8b4d493443de
Bruno de Oliveira <bruno.mktd@gmail.com>
2019-12-14 17:05:09 -0300

    Inserção de issue/label do github em repositorios

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,120 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  animation: ${rotate} 2s linear infinite;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-decoration: none;
+
+    p {
+      color: #7159c1;
+      margin-left: 5px;
+      font-size: 16px;
+    }
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 14px;
+    color: #666;
+    line-height: 1.4;
+    text-align: center;
+    max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 20px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+  }
+
+  e + li {
+    margin-top: 10px;
+  }
+
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 2px solid #eee;
+  }
+
+  div {
+    flex: 1;
+    margin-left: 15px;
+
+    strong {
+      font-size: 16px;
+
+      a {
+        text-decoration: none;
+        color: #333;
+
+        &:hover {
+          color: #7159c1;
+        }
+      }
+
+      span {
+        background: #eee;
+        color: #333;
+        border-radius: 2px;
+        font-size: 12px;
+        font-weight: 600;
+        height: 20px;
+        padding: 3px 4px;
+        margin-left: 10px;
+      }
+    }
+
+    p {
+      margin-top: 5px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
